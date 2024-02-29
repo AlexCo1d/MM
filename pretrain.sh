16abf3608d487191ffb94cb5a5273d1119d0583a
Alex <alexyhzou@qq.com>
2024-02-29 15:34:21 -0500

    pretrain

--- pretrain.sh
+++ pretrain.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=2 --master_port 11451 main_pretrain.py \
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nodes=1 --nproc_per_node=2 main_pretrain.py \
     --num_workers 10 \
     --accum_iter 2 \
     --batch_size 32 \
@@ -9,4 +9,4 @@ CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python -m torch.distributed.launch --
     --blr 1.5e-4 --weight_decay 0.05 \
     --resume /home/data/Jingkai/alex/weight/MM.pth \
     --data_path /home/data/Jingkai/alex/mimic \
-    --output_dir /home/data/Jingkai/alex/pretrain \
+    --output_dir /home/data/Jingkai/alex/pretrain
