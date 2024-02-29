9923f414251a3f22740be964404d93821a67be35
Alex <alexyhzou@qq.com>
2024-02-29 14:16:18 -0500

    start improve and add function

--- pretrain.sh
+++ pretrain.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=2 --master_port 11451 main_pretrain.py \
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=2 --use_env --master_port 11451 main_pretrain.py \
     --num_workers 10 \
     --accum_iter 2 \
     --batch_size 32 \
