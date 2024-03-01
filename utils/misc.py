6ddccfff30b08a3650711d78341ccc3618b6350c
Alex <alexyhzou@qq.com>
2024-02-29 19:19:43 -0500

    pretrain

--- utils/misc.py
+++ utils/misc.py
@@ -224,6 +224,8 @@ def init_distributed_mode(args):
         os.environ['WORLD_SIZE'] = str(args.world_size)
         # ["RANK", "WORLD_SIZE", "MASTER_ADDR", "MASTER_PORT", "LOCAL_RANK"]
     elif 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        print('use env')
+        print(os.environ['WORLD_SIZE'])
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
         args.gpu = int(os.environ['LOCAL_RANK'])
