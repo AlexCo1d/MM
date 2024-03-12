8ec32da556ae5b834acf2ee74f68933e4184c454
Alex <alexyhzou@qq.com>
2024-03-11 21:43:02 -0400

    VQA task complete

--- classification/finetuning_100percent.sh
+++ classification/finetuning_100percent.sh
@@ -1,4 +1,6 @@
-CUDA_VISIBLE_DEVICES=1 python3 train.py --name mm --stage train --model vit_base_patch16 --model_type ViT-B_16 --num_classes 14 \
+export CUDA_VISIBLE_DEVICES=0,1;
+export OMP_NUM_THREADS=1;
+python -m torch.distributed.launch --nnodes=1 --master_port 12345 --nproc_per_node=2  --name mm --stage train --model vit_base_patch16 --model_type ViT-B_16 --num_classes 14 \
     --pretrained_path "/home/data/Jingkai/alex/pretrain/checkpoint-99.pth" --dataset_path '/home/data/Jingkai/alex/nihxray' \
     --output_dir "/home/data/Jingkai/alex/finetuning_outputs/nihxray" --data_volume '100' --num_steps 200000  --eval_batch_size 512 --img_size 224 \
     --learning_rate 3e-3 --warmup_steps 5000 --fp16 --fp16_opt_level O2 --train_batch_size 96
