1d7c53153a885214cfc18ccac355e8568e7d1294
Alex <alexyhzou@qq.com>
2024-03-16 13:53:07 -0400

    VQA task complete

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -42,22 +42,22 @@ def train_one_epoch(model: torch.nn.Module,
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
         with torch.cuda.amp.autocast():
             loss, _, _ = model(batch, mask_ratio=mask_ratio)
-            loss_values= []
-            loss_values_reduce=[]
-            loss_dict= {}
+            loss_values = []
+            loss_values_reduce = []
+            loss_dict = {}
             for i in range(len(loss)):
                 loss_values.append(loss[i].item())
-            loss = torch.sum(loss)
+            loss = torch.sum(torch.stack(loss), dim=0)
             loss = loss / accum_iter
             loss_scaler(loss, optimizer, parameters=model.parameters(),
                         update_grad=(data_iter_step + 1) % accum_iter == 0)
-            
+
             if (data_iter_step + 1) % accum_iter == 0:
                 optimizer.zero_grad()
 
             torch.cuda.synchronize()
             for i, l in enumerate(loss_values):
-                loss_dict={'loss'+str(i+1): l}
+                loss_dict = {'loss' + str(i + 1): l}
             metric_logger.update(**loss_dict)
 
             lr = optimizer.param_groups[0]["lr"]
@@ -66,7 +66,6 @@ def train_one_epoch(model: torch.nn.Module,
             for l in loss_values:
                 loss_values_reduce.append(misc.all_reduce_mean(l))
 
-
             if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
                 """ We use epoch_1000x as the x-axis in tensorboard.
                 This calibrates different curves when batch size changes.
@@ -74,7 +73,7 @@ def train_one_epoch(model: torch.nn.Module,
                 epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
 
                 for i, l in enumerate(loss_values_reduce):
-                    log_writer.add_scalar('train_loss'+str(i+1), l, epoch_1000x)
+                    log_writer.add_scalar('train_loss' + str(i + 1), l, epoch_1000x)
                 log_writer.add_scalar('lr', lr, epoch_1000x)
 
     # gather the stats from all processes
