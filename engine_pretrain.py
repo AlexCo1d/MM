17f3d39be1e435c3fdd38c43cb97c28727f5fe37
Alex <alexyhzou@qq.com>
2024-03-19 16:03:19 -0400

    VQA task complete

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -36,12 +36,12 @@ def train_one_epoch(model: torch.nn.Module,
         print('log_dir: {}'.format(log_writer.log_dir))
 
     mask_ratio = args.mask_ratio
-    for data_iter_step, batch in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+    for data_iter_step, image, text in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
         # we use a per iteration (instead of per epoch) lr scheduler
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
         with torch.cuda.amp.autocast():
-            loss, _, _ = model(batch, mask_ratio=mask_ratio)
+            loss, _, _ = model(image, text, mask_ratio=mask_ratio)
             loss_values = []
             loss_values_reduce = []
             loss_dict = {}
