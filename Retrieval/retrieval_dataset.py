6f85cbb6c2827a2f0276472abcf200d659234efa
Alex <alexyhzou@qq.com>
2024-05-07 17:37:17 -0400

    VQA task complete

---
+++ Retrieval/retrieval_dataset.py
@@ -0,0 +1,41 @@
+import PIL
+import pandas as pd
+import json
+import os
+
+from torch.utils.data import Dataset
+
+
+class retrieval_dataset(Dataset):
+    def __init__(self, data_path):
+        with open(os.path.join(data_path, f'candidate.csv')) as f:
+            self.data = pd.read_csv(f)
+        with open(os.path.join(data_path, f'IR_query.json')) as f:
+            self.IR_query = pd.read_csv(f)
+        with open(os.path.join(data_path, f'TR_query.json')) as f:
+            self.TR_query = pd.read_csv(f)
+        self.data_path = data_path
+        self.text = []
+        self.image = []
+        txt_id = 0
+        for img_id in range(len(self.IR_query)):
+            sample=self.IR_query[img_id]
+            img= PIL.Image.open(os.path.join(data_path, sample["Path"])).convert('RGB')
+            img.resize((224, 224))
+            self.image.append(img)
+        for text_id in range(len(self.IR_query)):
+            self.text.append(self.TR_query[text_id]["Text"])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        img_path = os.path.join(self.data_path, sample['Path'])
+        img = PIL.Image.open(img_path).convert('RGB')
+        img.resize((224, 224))
+        item={
+            'image': img,
+            'idx':idx
+        }
+        return item
---
+++ Retrieval/task.py
@@ -0,0 +1,131 @@
+import argparse
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+from model import Former_Retrieval
+import Utils.misc as misc
+from Retrieval.retrieval_dataset import retrieval_dataset
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint', type=str, default="./LLaMA/checkpoint-12600")
+    parser.add_argument('--data_path', type=str, default="")
+    parser.add_argument('--distributed', type=bool, default=True)
+    parser.add_argument('--device', default='cuda')
+    args = parser.parse_args()
+    if args.distributed:
+        misc.init_distributed_mode(args)
+
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    dataset = retrieval_dataset(args.data_path)
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False, num_workers=5, pin_memory=True)
+
+    model = Former_Retrieval.Former_RT()
+    model = model.to(device)
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+    model.eval()
+    ret = model.compute_sim_matrix(dataloader)
+    if misc.is_main_process():
+        _report_metrics(ret,args)
+
+
+@torch.no_grad()
+def _report_metrics(ret, args, txt2img, img2txt):
+    scores_i2t = ret["i2t"] # num_image x num_candidates
+    scores_t2i = ret["t2i"] # num_text x num_candidates
+    scores_i2i = ret["i2i"] # num_image x num_candidates
+    with open(os.path.join(args.data_path, f'candidate.csv')) as f:
+        candidate = pd.read_csv(f)
+    with open(os.path.join(args.data_path, f'IR_query.json')) as f:
+        IR_query = pd.read_csv(f)
+    with open(os.path.join(args.data_path, f'TR_query.json')) as f:
+        TR_query = pd.read_csv(f)
+
+    # # Images->Text
+    # ranks = np.zeros(scores_i2t.shape[0])
+    # for index, score in enumerate(scores_i2t):
+    #     inds = np.argsort(score)[::-1]
+    #     # Score
+    #     rank = 1e20
+    #     for i in img2txt[index]:
+    #         tmp = np.where(inds == i)[0][0]
+    #         if tmp < rank:
+    #             rank = tmp
+    #     ranks[index] = rank
+    #
+    # # Compute metrics
+    # tr1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    # tr5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    # tr10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    #
+    # # Text->Images
+    # ranks = np.zeros(scores_t2i.shape[0])
+    #
+    # for index, score in enumerate(scores_t2i):
+    #     inds = np.argsort(score)[::-1]
+    #     ranks[index] = np.where(inds == txt2img[index])[0][0]
+    #
+    # # Compute metrics
+    # ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    # ir5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    # ir10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    #
+    # tr_mean = (tr1 + tr5 + tr10) / 3
+    # ir_mean = (ir1 + ir5 + ir10) / 3
+    # r_mean = (tr_mean + ir_mean) / 2
+    #
+    # agg_metrics = (tr1 + tr5 + tr10) / 3
+    #
+    # eval_result = {
+    #     "txt_r1": tr1,
+    #     "txt_r5": tr5,
+    #     "txt_r10": tr10,
+    #     "txt_r_mean": tr_mean,
+    #     "img_r1": ir1,
+    #     "img_r5": ir5,
+    #     "img_r10": ir10,
+    #     "img_r_mean": ir_mean,
+    #     "r_mean": r_mean,
+    #     "agg_metrics": agg_metrics,
+    # }
+    # with open(
+    #         os.path.join(registry.get_path("output_dir"), "evaluate.txt"), "a"
+    # ) as f:
+    #     f.write(json.dumps(eval_result) + "\n")
+    def compute_precision_at_k(scores, query, candidate, k=1):
+        precisions = []
+        for idx, score in enumerate(scores):
+            top_k_indices = np.argsort(score)[-k:][::-1]  # 从高到低
+            matched = 0
+            for top_idx in top_k_indices:
+                # 检查候选项中的one-hot编码是否与查询匹配
+                if candidate.iloc[top_idx][query['variable'].iloc[idx]] == 1:
+                    matched += 1
+            precision = matched / k
+            precisions.append(precision)
+        return np.mean(precisions)
+
+    eval_result = {
+        "t2i_r1": compute_precision_at_k(scores_t2i, IR_query, k=1),
+        "t2i_r5": compute_precision_at_k(scores_t2i, IR_query, k=5),
+        "t2i_r10": compute_precision_at_k(scores_t2i, IR_query, k=10),
+        "i2i_r1": compute_precision_at_k(scores_i2i, TR_query, k=1),
+        "i2i_r5": compute_precision_at_k(scores_i2i, TR_query, k=5),
+        "i2i_r10": compute_precision_at_k(scores_i2i, TR_query, k=10),
+    }
+
+    print(eval_result)
+    return eval_result
+
+
+if __name__ == "__main__":
+    main()
