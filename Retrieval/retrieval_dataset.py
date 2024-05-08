3ab8b6c798f8d3c9748f6347ed97d26241dfcdd3
Alex <alexyhzou@qq.com>
2024-05-08 18:33:11 -0400

    VQA task complete

--- Retrieval/retrieval_dataset.py
+++ Retrieval/retrieval_dataset.py
@@ -1,8 +1,8 @@
-import PIL
 import pandas as pd
 import json
 import os
 import torchvision.transforms as transforms
+from PIL import Image
 from torch.utils.data import Dataset
 
 
@@ -17,15 +17,14 @@ class retrieval_dataset(Dataset):
         self.data_path = data_path
         self.text = []
         self.image = []
-        self.transform= transforms.Compose([
-            transforms.Resize(224),
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224), interpolation=Image.BICUBIC),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         for img_id in range(len(self.IR_query)):
-            sample=self.IR_query.iloc[img_id]
-            img= PIL.Image.open(os.path.join(data_path, sample["Path"])).convert('RGB')
-            img.resize((224, 224))
+            sample = self.IR_query.iloc[img_id]
+            img = Image.open(os.path.join(data_path, sample["Path"])).convert('RGB')
             self.image.append(self.transform(img))
         for text_id in range(len(self.TR_query)):
             self.text.append(self.TR_query.iloc[text_id]["Text"])
@@ -36,10 +35,9 @@ class retrieval_dataset(Dataset):
     def __getitem__(self, idx):
         sample = self.data.iloc[idx]
         img_path = os.path.join(self.data_path, sample['Path'])
-        img = PIL.Image.open(img_path).convert('RGB')
-        img.resize((224, 224))
-        item={
+        img = Image.open(img_path).convert('RGB')
+        item = {
             'image': self.transform(img),
-            'idx':idx
+            'idx': idx
         }
         return item
