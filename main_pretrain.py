73764df6c85cdca722f7697fc43cff668f16c030
Alex <alexyhzou@qq.com>
2024-05-20 16:12:41 -0400

    re-train!

--- main_pretrain.py
+++ main_pretrain.py
@@ -185,7 +185,7 @@ def main(args):
 
     # following timm: set wd as 0 for bias and norm layers
     param_groups = Utils.lr_decay.add_weight_decay(model_without_ddp, args.weight_decay)
-    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
+    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.98))
     print(optimizer)
     loss_scaler = NativeScaler()
 
