9fd330b4fa8657a6b933fb4ce34473dcbd742c96
Alex <alexyhzou@qq.com>
2024-06-16 11:05:20 -0400

    re-train!

--- VQA/train.py
+++ VQA/train.py
@@ -194,15 +194,15 @@ def main(args):
                 }
                 prefix = args.checkpoint.split('/')[-1].split('.')[0]
                 # for evaluation and output the result
-                if args.output_dir and epoch >= 20 and (epoch % args.eval_freq == 0 or epoch == args.epochs - 1):
+                if args.output_dir and epoch >= 10 and (epoch % args.eval_freq == 0 or epoch == args.epochs - 1):
                     torch.cuda.empty_cache()
                     torch.save(save_obj, os.path.join(args.output_dir, '%s_%s_%02d.pth' % (prefix, args.dataset_use, epoch)))
                     vqa_result = evaluation(model, test_loader, device, args)
                     json.dump(vqa_result,
                               open(os.path.join(args.result_dir, '%s_vqa_result_%s.json' % (prefix, epoch)), 'w'))
-                    acc = compute_vqa_acc(vqa_result, args=args, dataloader=test_loader, epoch=checkpoint['epoch'])
+                    acc = compute_vqa_acc(vqa_result, args=args, dataloader=test_loader, epoch=epoch)
                     print({'epoch:':epoch,'acc:':acc})
-                    json.dump({'epoch:':epoch,'acc:':acc},
+                    json.dump({'epoch:':epoch,'acc:':dict(list(acc.items())[1:])},
                               open(os.path.join(args.result_dir, 'vqa_metric.json'), 'a'))
                 torch.save(save_obj, os.path.join(args.output_dir, 'last_epoch_weight.pth'))
 
