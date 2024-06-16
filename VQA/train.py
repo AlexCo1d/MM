777d51b56d9f3e22fa5ca61b119a9834ca32aabd
Alex <alexyhzou@qq.com>
2024-06-15 23:26:51 -0400

    re-train!

--- VQA/train.py
+++ VQA/train.py
@@ -202,6 +202,7 @@ def main(args):
                     json.dump(vqa_result,
                               open(os.path.join(args.result_dir, '%s_vqa_result_%s.json' % (prefix, epoch)), 'w'))
                     acc = compute_vqa_acc(vqa_result, args=args, dataloader=test_loader, epoch=checkpoint['epoch'])
+                    print({'epoch:':epoch,'acc:':acc})
                     acc_list.append({'epoch:':epoch,'acc:':acc})
                 torch.save(save_obj, os.path.join(args.output_dir, 'last_epoch_weight.pth'))
 
