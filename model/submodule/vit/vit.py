532d0b373f7021076235b1fbc36a0a51d609eb40
Alex <alexyhzou@qq.com>
2024-06-18 00:20:11 -0400

    re-train!

--- model/submodule/vit/vit.py
+++ model/submodule/vit/vit.py
@@ -5,7 +5,7 @@ import torch
 from timm.layers import DropPath, Mlp
 from torch import nn
 from timm.models.vision_transformer import PatchEmbed
-
+from timm.models.layers import trunc_normal_, DropPath
 from model.submodule.vit.eva_vit import interpolate_pos_embed
 
 
@@ -95,31 +95,82 @@ def get_ViT(vit_path='', img_size=224, **kwargs):
     return model
 
 
-class ViT(timm.models.vision_transformer.VisionTransformer):
-    """ Vision Transformer with support for global average pooling
+class ViT(nn.Module):
+    """ Vision Transformer
+    A PyTorch impl of : `An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale`  -
+        https://arxiv.org/abs/2010.11929
     """
 
-    def __init__(self, embed_dim=768, depth=12, num_heads=12, **kwargs):
-        super(ViT, self).__init__(embed_dim=768, depth=12, num_heads=12, **kwargs)
-        self.blocks = nn.ModuleList([
-            Block(embed_dim, num_heads, qkv_bias=True)
-            for i in range(depth)])
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 num_heads=12, mlp_ratio=4., qkv_bias=True, qk_scale=None, representation_size=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0., norm_layer=None):
+        """
+        Args:
+            img_size (int, tuple): input image size
+            patch_size (int, tuple): patch size
+            in_chans (int): number of input channels
+            num_classes (int): number of classes for classification head
+            embed_dim (int): embedding dimension
+            depth (int): depth of transformer
+            num_heads (int): number of attention heads
+            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
+            qkv_bias (bool): enable bias for qkv if True
+            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
+            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
+            drop_rate (float): dropout rate
+            attn_drop_rate (float): attention dropout rate
+            drop_path_rate (float): stochastic depth rate
+            norm_layer: (nn.Module): normalization layer
+        """
+        super().__init__()
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
 
-    def forward_features(self, x):
+        self.patch_embed = PatchEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+
+        trunc_normal_(self.pos_embed, std=.02)
+        trunc_normal_(self.cls_token, std=.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def forward(self, x, register_blk=-1):
         B = x.shape[0]
         x = self.patch_embed(x)
 
         cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
         x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed
+
+        x = x + self.pos_embed[:, :x.size(1), :]
         x = self.pos_drop(x)
 
-        hidden_features = []
-        # apply Transformer blocks
         for i, blk in enumerate(self.blocks):
-            x = blk(x, i == 11)
-            hidden_features.append(x)
-
+            x = blk(x, register_blk == i)
         x = self.norm(x)
 
         return x
