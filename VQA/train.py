7120a5602cd9e0b8309d9dd6615d312445605a6c
Alex <alexyhzou@qq.com>
2024-06-19 11:40:29 -0400

    re-train!

--- VQA/train.py
+++ VQA/train.py
@@ -141,7 +141,7 @@ def main(args):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         if args.deepspeed:
             import deepspeed
-            model, optimizer, _, _ = deepspeed.initialize(args=args, model=model, optimizer=optimizer)
+            model, optimizer, _, _ = deepspeed.initialize(args=args, model=model, optimizer=optimizer, model_parameters=model.parameters())
 
         model_without_ddp = model.module
 
