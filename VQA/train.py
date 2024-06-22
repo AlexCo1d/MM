4dc5b22028b7dbeba6cdc84f1b32c073000d4670
Alex <alexyhzou@qq.com>
2024-06-22 18:14:52 -0400

    re-train!

--- VQA/train.py
+++ VQA/train.py
@@ -151,8 +151,10 @@ def main(args):
     start_epoch = 0
     if args.checkpoint:
         checkpoint = torch.load(args.checkpoint, map_location='cpu')
-
-        msg = model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        model_dict = model_without_ddp.state_dict()
+        pretrained_dict = {k: v for k, v in checkpoint['model'].items() if k in model_dict and v.shape==model_dict[k].shape}
+        model_dict.update(pretrained_dict)
+        msg = model_without_ddp.load_state_dict(model_dict)
         print('load checkpoint from %s' % args.checkpoint)
         print(msg)
         if 'optimizer' in checkpoint and args.load_optim:
